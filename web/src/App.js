2b0dac331ea6f14dc7954f6257879efc7f89817d
unknown <fabio.nsousa1@gmail.com>
2024-06-02 16:28:15 -0300

    feat: ajusted

--- web/src/App.js
+++ web/src/App.js
@@ -8,9 +8,9 @@ function App() {
   // listName =  array de objetos
   const listName = [
     { name:  'Jessica', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', undername: 'anastacio', age: 35},
-    { name:  'Davi', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', age: 15},
-    { name:  'Fabiççççço', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', age: 46},
-    { name:  'Leo', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', age: 73},
+    { name:  'Davi', img: 'https://sgimage.netmarble.com/images/netmarble/sololv/20240318/fxbt1710761341766.jpg', age: 15},
+    { name:  'Fabio', img: 'https://tm.ibxk.com.br/2024/01/09/09113345060123.jpg?ims=1200x675', age: 46},
+    { name:  'Leo', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjlEt8eqGTapQZ1DauAYpozboLgqwkab45-g&s', age: 73},
     { name:  'Erick', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', subtitle: 'sung jin woo', age: 26},
     { name:  'Sandra', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', age: 37},
     { name:  'Jorge', img: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/solo_leveling_o_que_e__w24vo9.jpg', age: 52}, 
